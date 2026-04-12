4d9fbf7bc7c0a0bd9df7d6595da062de1e3392f9
enchanted-sword <xlucas4956@gmail.com>
2026-04-12 12:52:16 -0300

    Ditto

--- src/scripts/emojiFix.js
+++
@@ -1,4 +0,0 @@
-const customClass = 'tailfeather-emojifix';
-
-export const main = async () => document.documentElement.append(Object.assign(document.createElement('link'), { rel: 'stylesheet', className: customClass, href: 'https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&display=swap' }));
-export const clean = async () => document.querySelectorAll(`.${customClass}`).forEach(s => s.remove());
