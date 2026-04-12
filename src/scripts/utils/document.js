1d2a45d0815cb10a68b542f197de7ed0b72cae5d
enchanted-sword <xlucas4956@gmail.com>
2026-04-12 12:52:24 -0300

    Fix book rendering

--- src/scripts/utils/document.js
+++ src/scripts/utils/document.js
@@ -5,19 +5,26 @@ export const postSelector = 'article[data-post-id]';
 const bookShadowHostId = 'book-shadow-host';
 
 export const dynamicStyle = (customClass = '') => ({
-  attached: false,
+  _shadowWatch(_, observer) {
+    observer.disconnect();
+    document.getElementById(bookShadowHostId).shadowRoot.append(this.styleSheet.cloneNode(true));
+  },
   styleSheet: noact({
     tag: 'style',
     className: `tf-dynamicStyle ${customClass}`
   }),
   get textContent() { return this.styleSheet.textContent; },
   set textContent(style) {
-    if (!this.attached) this.attach();
+    if (!this.styleSheet.isConnected) this.attach();
     this.styleSheet.textContent = style;
   },
   attach() {
     document.body.append(this.styleSheet);
-    document.getElementById(bookShadowHostId)?.shadowRoot?.append(this.styleSheet.cloneNode(true));
-    this.attached = true;
+    const bookHost = document.getElementById(bookShadowHostId);
+    if (bookHost?.shadowRoot) bookHost.shadowRoot.append(this.styleSheet.cloneNode(true));
+    else if (bookHost) {
+      const observer = new MutationObserver(this._shadowWatch.bind(this));
+      observer.observe(bookHost, { childList: true, subtree: true });
+    }
   }
 });
