af252b4572cb9aaec3e84c761ab34e7e16fab051
enchanted-sword <xlucas4956@gmail.com>
2026-04-24 10:40:31 -0300

    .beat timestamps

--- src/scripts/timestamps.js
+++ src/scripts/timestamps.js
@@ -5,16 +5,45 @@ import { noact } from './utils/noact.js';
 const { DateTime } = luxon;
 const customClass = 'tailfeather-timestamps';
 const timeSelector = 'span[data-ts]';
+const BEAT_LENGTH = 86.4;
 
-let formatOpts, opts;
+let formatOpts, opts, beats;
+
+// copied from cohost's @client/lib/beats.ts
+// lol
+
+function fromDateTime(date, centibeats = false) {
+  const bmtDate = date.setZone("UTC+1");
+  let beats =
+    (bmtDate.second + bmtDate.minute * 60 + bmtDate.hour * 3600) /
+    BEAT_LENGTH;
+
+  beats = centibeats ? beats % 1000 : Math.floor(beats % 1000);
+
+  return `@${beats
+    .toFixed(centibeats ? 2 : 0)
+    .padStart(centibeats ? 6 : 3, "0")}`;
+}
+
+function beatsFormat(dateTime) {
+  return `${dateTime
+    .setZone("UTC+1")
+    .toLocaleString(DateTime.DATE_MED)} ${fromDateTime(dateTime, true)}`;
+}
 
 const formatTimestamp = iso => {
+  const dateTime = DateTime.fromISO(iso);
   let time;
-  try {
-    time = DateTime.fromISO(iso).toLocaleString(formatOpts, opts);
-  } catch {
-    time = DateTime.fromISO(iso).toLocaleString(formatOpts); // fallback for invalid locale
+
+  if (beats) time = beatsFormat(dateTime);
+  else {
+    try {
+      time = dateTime.toLocaleString(formatOpts, opts);
+    } catch {
+      time = dateTime.toLocaleString(formatOpts); // fallback for invalid locale
+    }
   }
+
   return time;
 };
 
@@ -25,6 +54,10 @@ const addTimestamps = timeElements => timeElements.forEach(timeElement => {
     tag: 'time',
     className: `${customClass} ${inheritedClasses}`,
     title: DateTime.fromISO(iso).toLocaleString(DateTime.DATETIME_FULL),
+    dataset: {
+      relative: timeElement.textContext,
+      title: timeElement.title
+    },
     datetime: iso,
     children: formatTimestamp(iso),
   });
@@ -32,8 +65,20 @@ const addTimestamps = timeElements => timeElements.forEach(timeElement => {
   timeElement.replaceWith(timestamp);
 });
 
+const revertTimestamps = () => {
+  document.querySelectorAll(`.${customClass}`).forEach(timeElement => {
+    timeElement.replaceWith(noact({
+      tag: 'span',
+      className: timeElement.classList.value.replace(customClass, ''),
+      title: timeElement.dataset.title,
+      dataset: { ts: timeElement.datetime },
+      children: timeElement.dataset.relative
+    }))
+  });
+}
+
 export const main = async () => {
-  ({ formatOpts, opts } = await getOptions('timestamps'));
+  ({ formatOpts, opts, beats } = await getOptions('timestamps'));
 
   try { formatOpts = JSON.parse(`"${formatOpts}"`); }
   catch {
@@ -53,4 +98,5 @@ export const main = async () => {
 
 export const clean = async () => {
   mutationManager.stop(addTimestamps);
+  revertTimestamps();
 };
